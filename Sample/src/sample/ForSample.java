031922ccc00203d27df583178afd96884ff3c81c
MayuKataoka(片岡麻由) <mayu.kataoka.p191@dirbato.co.jp>
2025-05-23 18:39:51 +0900

    研修1日目

---
+++ Sample/src/sample/ForSample.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class ForSample {
+
+    public static void main(String[] args) {
+        var numbers = new int[] {1,2,3,4,5};
+        for(var number:numbers) {
+            System.out.println(number);
+        }
+       }
+}
